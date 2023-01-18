ec4479615d4e1692a844fb9741d7d7eeb0129f69
lerkush <lutfiyye@gmail.com>
2023-01-18 18:30:53 +0300

    added static assets,about pageand crispy forms

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,12 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #Third-party
+    "crispy_forms",
+    "crispy_bootstrap5",
     #Local
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
 ]
 
-
+# django-crispy-forms
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5" 
 
 
 MIDDLEWARE = [
@@ -126,6 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,8 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+
