066895ff98292bba6629c7eb8f3e41508283d7e5
lerkush <lutfiyye@gmail.com>
2023-01-21 19:26:30 +0300

    added book reviews

--- books/admin.py
+++ books/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Review
+
+class ReviewInline(admin.TabularInline):
+    model = Review
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ("title", "author","price")
+    inlines = [
+        ReviewInline,
+    ]
+    list_display = ("title", "author", "price",)
 
 admin.site.register(Book, BookAdmin)
-
--- books/tests.py
+++ books/tests.py
@@ -1,16 +1,31 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from .models import Book
+from .models import Book, Review
 
 class BookTests(TestCase):
 
     @classmethod
     def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="reviewuser",
+            email="reviewuser@email.com",
+            password="testpass123",
+        )
+    
         cls.book = Book.objects.create(
             title="Harry Potter",
             author="JK Rowling",
             price="25.00",
+        )
+    
+        cls.review = Review.objects.create(
+            book=cls.book,
+            author=cls.user,
+            review="An excellent review",
+
+        
         )
     
     def test_book_listing(self):
@@ -30,5 +45,6 @@ class BookTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "Harry Potter")
+        self.assertContains(response, "An excellent review")
         self.assertTemplateUsed(response, "books/book_detail.html")
     
